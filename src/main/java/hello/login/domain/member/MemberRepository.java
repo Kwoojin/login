98ac05c0a9dbe9e3bee32d1397f985f004d8fb68
WoojinKim <dnwls488@naver.com>
2022-06-02 10:49:15 +0900

    member 추가

---
+++ src/main/java/hello/login/domain/member/MemberRepository.java
@@ -0,0 +1,15 @@
+package hello.login.domain.member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+
+    public Member save(Member member);
+
+    public Member findById(Long id);
+
+    public Optional<Member> findByLoginId(String loginId);
+
+    public List<Member> findAll();
+}
